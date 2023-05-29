f578db31107ff66b57a0792f682a0267ac68f8d7
Tolea01 <anatoliecurbat@gmail.com>
2023-05-29 21:05:07 +0300

    modal

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -1,8 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LaunchIcon from '@mui/icons-material/Launch';
+import { Button, Box, Container, Stack } from '@mui/material';
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+
+const classes = {
+  overlay: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'black',
+    opacity: '.7',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: 3,
+    transition: 'all .7s',
+  },
+
+  closeButton: {
+    cursor: 'pointer',
+    color: 'gray',
+    position: 'absolute',
+    right: '10px',
+    top: '10px',
+  },
+
+  inactive: {
+    visibility: 'hidden',
+    opacity: 0,
+    transition: 'all .7s',
+  },
+
+  modal: {
+    width: '500px',
+    height: '400px',
+    backgroundColor: 'white',
+    position: 'absolute',
+    top: '100px',
+    left: '50%',
+    transform: 'translateX(-50%)',
+    zIndex: 5,
+    transition: 'all .7s',
+  }
+}
+
 
 function App() {
+  const [show, setShow] = useState<boolean>(false);
+
   return (
-    <h1>Hello World!!!</h1>
+    <>
+      <Container maxWidth='xs'>
+        <Stack>
+          <Button
+            variant='outlined'
+            sx={{ marginTop: '300px' }}
+            onClick={(): void => setShow(true)}
+            startIcon={<LaunchIcon />}>
+            Open Modal
+          </Button >
+        </Stack>
+      </Container >
+      <Box sx={show ? classes.overlay : null} />
+      <Box sx={show ? classes.modal : null} >
+        <CloseOutlinedIcon
+          sx={classes.closeButton}
+          onClick={(): void => setShow(!show)}
+        />
+      </Box>
+    </>
   );
 }
 
